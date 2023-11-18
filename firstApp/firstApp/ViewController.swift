de8edf3190652272cb6b8f41ef3472734ba4122e
Rakhat <rahat200215@gmail.com>
2023-11-19 04:30:53 +0600

    iOS Programming

--- firstApp/firstApp/ViewController.swift
+++ firstApp/firstApp/ViewController.swift
@@ -13,24 +13,67 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var secondTextField: UITextField!
+
+    var operation = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func touched(_ sender: Any) {
-        //label.text = textField.text
+    @IBAction func touchedPlus(_ sender: Any) {
+        
+        operation = "+"
+    }
+    
+    @IBAction func touchedMinus(_ sender: Any) {
+        
+        operation = "-"
+    }
+    
+    @IBAction func touchedMultiply(_ sender: Any) {
+        
+        operation = "*"
+    }
+    
+    @IBAction func touchedDivide(_ sender: Any) {
+        
+        operation = "/"
+    }
+    
+    @IBAction func touchedEqual(_ sender: Any) {
         
         let a = textField.text!
         
         let b = secondTextField.text!
         
-        let sum = Int(a)! + Int(b)!
-        
-        label.text = "sum = \(textField.text!) + \(secondTextField.text!) = \(sum)"
+        if a.isEmpty && b.isEmpty
+        {
+            textField.placeholder = "Put number"
+            secondTextField.placeholder = "Put number"
+        }
+            
+        if operation == "+"
+        {
+            let sum = Int(a)! + Int(b)!
+            label.text = "result = \(sum)"
+        }
+        else if operation == "-"
+        {
+            let minus = Int(a)! - Int(b)!
+            label.text = "result = \(minus)"
+        }
+        else if operation == "*"
+        {
+            let multiply = Int(a)! * Int(b)!
+            label.text = "result = \(multiply)"
+        }
+        else if operation == "/"
+        {
+            let divide = Int(a)! / Int(b)!
+            label.text = "result = \(divide)"
+        }
     }
-    
-    
 }
 
