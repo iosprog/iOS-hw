780756a1fd3d6084444dd8f5f1525d4a7d89026b
Rakhat <rahat200215@gmail.com>
2023-11-22 21:45:32 +0600

    iOS prog

---
+++ tableView/tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by РАХАТ  on 21.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
