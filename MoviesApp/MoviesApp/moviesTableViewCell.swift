50b102155f7f25a05d06a87f868bd6ceb040c9e3
Rakhat <rahat200215@gmail.com>
2024-01-31 14:40:04 +0600

    iOS Prog

---
+++ MoviesApp/MoviesApp/moviesTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  moviesTableViewCell.swift
+//  MoviesApp
+//
+//  Created by РАХАТ  on 23.01.2024.
+//
+
+import UIKit
+
+class moviesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieGenreLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
