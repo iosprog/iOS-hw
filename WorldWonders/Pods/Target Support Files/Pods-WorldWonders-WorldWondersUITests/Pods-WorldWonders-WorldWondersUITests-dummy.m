ca7e3419c767144c897d275eb47f86bf016c0b02
Rakhat <rahat200215@gmail.com>
2024-05-24 23:59:43 +0500

    iOS Dev

--- WorldWonders/Pods/Target Support Files/Pods-WorldWonders-WorldWondersUITests/Pods-WorldWonders-WorldWondersUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WorldWonders_WorldWondersUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WorldWonders_WorldWondersUITests
-@end
--- WorldWonders/Pods/Target Support Files/Pods-WorldWondersTests/Pods-WorldWondersTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WorldWondersTests : NSObject
-@end
-@implementation PodsDummy_Pods_WorldWondersTests
-@end
